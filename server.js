c27463d3025865dcc13efd7f917e7452d5e19051
maciejsto <maciejsto@gmail.com>
2014-06-02 20:52:30 +0200

    sample server example

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 /**
  * Created by syzer on 5/28/2014.
  */
+
+var http = require('http');
+
+
+	http.createServer(function (req, res) {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end('Hello World\n');
+	}).listen(1337, '127.0.0.1');
+	
+console.log('Server running at http://127.0.0.1:1337/');
