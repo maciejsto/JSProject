c2468722ee0d909164858867557eaa7cc58f85ab
maciejsto <maciejsto@gmail.com>
2014-06-06 11:48:04 +0200

    update web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello World2222!');
 });
 
 var port = Number(process.env.PORT || 5000);
