f52b98139a36f6f96840cbfaff18373162d524f0
maciejsto <maciejsto@gmail.com>
2014-06-06 21:05:42 +0200

    web

--- web.js
+++ web.js
@@ -7,16 +7,25 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World2222!');
+  //res.send('Hello World2!');
+  res.render('helloworld', { title: 'Hello, World!' })
   
-  //res.send(fetch_data(c));
+  db.collection('mongodb').find().toArray(function(err, data){
+				console.log(data);
+	  res.send(data);
+  });
+  
+  
+  
+  
+  
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
+var global = "";
 
  var mongo = require('mongodb');
 
@@ -28,27 +37,44 @@ app.listen(port, function() {
 //  //console.log(process.env.MONGOLAB_URI);
  
  var db_uri = "mongodb://heroku_app26086154:pmkqvnf3j0tk1j7umugikfdie@ds041218.mongolab.com:41218/heroku_app26086154";
+ 
+ 
  	mongo.Db.connect(db_uri, function (err, db) {
  	if (!err){
  		console.log("you are connected to mongodb on heroku :D");
  	}
-   if (!db.collection('mongodb')){ 	
+    if (!db.collection('mongodb')){ 	
  	  db.collection('mongodb', function(er, collection) {
  	    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
  	    });
  	  });
-   }
-   //d = "";
-   var c = db.collection('mongodb').find().toArray(function(err,data){
- 	  console.log(data);
- 	 app.get('/', function(req, res) {
- 		  res.send(data);
- 		  
- 		  //res.send(fetch_data(c));
- 		});
-	 
-   });
+    }
   
+  
+	function fetch(){
+		
+			db.collection('mongodb').find().toArray(function(err, data){
+				//console.log(data);
+			});
+	
+	}
+	var c = db.collection('mongodb', function(er, collection) {
+ 	    var tmp = [];
+		var d = collection.find().toArray(function(er,rs) {
+ 	    	//console.log(rs);
+ 	    	tmp = rs;
+ 	    });
+ 	    
+ 	  });
+	
+	//console.log(c);
+	//fetch();
+
+ 	
+	
+
+	
   
  });
+
 
