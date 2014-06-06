bd1ca4ce15f67993d6968b3c8e96e9a35a8c1846
maciejsto <maciejsto@gmail.com>
2014-06-06 12:24:30 +0200

    working with mongodb

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var mongo = require('mongodb');
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -12,4 +13,17 @@ app.get('/', function(req, res) {
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
 });
