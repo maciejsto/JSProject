15b73123b3a927463a7b8ada68f7d296b6b74d76
maciejsto <maciejsto@gmail.com>
2014-06-06 13:33:57 +0200

    working with mongo

--- web.js
+++ web.js
@@ -7,6 +7,8 @@ app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World2222!');
+  
+  //res.send(fetch_data(c));
 });
 
 var port = Number(process.env.PORT || 5000);
@@ -14,19 +16,39 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-var mongo = require('mongodb');
+//var mongo = require('mongodb');
+//
+//var mongoUri = process.env.MONGOLAB_URI ||
+//  process.env.MONGOHQ_URL ||
+//  'mongodb://localhost/mydb';
 
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
-
- console.log(mongoUri);
- console.log(process.env.MONGOLAB_URI);
+ //console.log(mongoUri);
+ //console.log(process.env.MONGOLAB_URI);
  
-var db_uri = "mongodb://heroku_app26080265:j20c7r9l75s2t59beq2ivek63f@ds039768.mongolab.com:39768/heroku_app26080265";
-mongo.Db.connect(db_uri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });
-});
+//var db_uri = "mongodb://heroku_app26086154:pmkqvnf3j0tk1j7umugikfdie@ds041218.mongolab.com:41218/heroku_app26086154";
+//mongo.Db.connect(db_uri, function (err, db) {
+//	if (!err){
+//		console.log("you are connected to mongodb on heroku :D");
+//	}
+//  //if (!db.collection('mongodb')){ 	
+//	  db.collection('mongodb', function(er, collection) {
+//	    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+//	    });
+//	  });
+//  //}
+//  //d = "";
+//  var c = db.collection('mongodb').find().toArray(function(err,data){
+//	  console.log(data);
+//	  app.get('/', function(req, res) {
+//		  res.send(data);
+//		  
+//		  res.send(data);
+//		});
+//	 
+//  });
+//  
+//  
+//  
+//  
+//  
+//});
