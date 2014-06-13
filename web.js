8ccea57bf002245f1dcf92ecdec13afb35039bfb
syzer <syzer3@gmail.com>
2014-06-14 00:01:23 +0200

    formatting

--- web.js
+++ web.js
@@ -6,26 +6,26 @@ var db_uri = sm.get('config').mongoDb.uri;
 var mongoose = sm.get('mongoose');
 var when = sm.get('when');
 var astronautModel = sm.get('astronaut');
-var express=require('express');
+var express = require('express');
 var fs = require('fs');
 //var routes = require('./src/routes');
 
 
 var astroData = {
-	    name : 'John Schimmel',
-	    skills : ['floating','repairing satellites'],
-	    walkedOnMoon : false
-	};
+    name: 'John Schimmel',
+    skills: ['floating', 'repairing satellites'],
+    walkedOnMoon: false
+};
 
 
 fs.readdirSync('./src/backend/controllers').forEach(function (file) {
-	  if(file.substr(-3) == '.js') {
-		  console.log("routes",file);
-	      route = require('./src/backend/controllers/' + file);
-	      console.log(route);
-	      route.controller(app, sm.get('users'));
-	  }
-	});
+    if (file.substr(-3) == '.js') {
+        console.log("routes", file);
+        route = require('./src/backend/controllers/' + file);
+        console.log(route);
+        route.controller(app, sm.get('users'));
+    }
+});
 
 
 var port = Number(process.env.PORT || 5000);
