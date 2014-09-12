78923a5fb0f27938ce0c99611ef79233cfcb77d0
maciejsto <maciejsto@gmail.com>
2014-09-12 12:33:30 +0200

    refacotirng routes and controllers, adding socket.io functionallity

--- src/backend/config/serviceConfig.js
+++ src/backend/config/serviceConfig.js
@@ -10,8 +10,9 @@ var services = {
     app: function addService(sm) {
         var express = sm.get('express');
         var app = express();
-        app.use(sm.get('bodyParser'));
+        app.use(sm.get('bodyParser'))
         app.use(sm.get('methodOverride'));
+
         var allowCrossDomain = function (req, res, next) {
             res.header('Access-Control-Allow-Origin', '*');//config.allowedDomains
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -21,6 +22,7 @@ var services = {
         app.use("/bower_components",express.static(ROOT_PATH + 'bower_components'));
         app.use(allowCrossDomain);
         app.use(express.static(ROOT_PATH + 'src/front'));
+
         return app;
     },
 
@@ -30,23 +32,39 @@ var services = {
             var SerialPort = require("serialport").SerialPort;
             var serialPort = new SerialPort(portName,
                 {
-                    bauderate: 9600,
-                });
+                    bauderate: 9600
+                },false);   //second param is openFlag by default set to true which means open port immediatelly
             return serialPort;
         };
     },
     arduinomodel: function addService(sm){
-        var COM3 = sm.get('config').Serial.port;
-        var serialPort = sm.get('serial')("COM3");
-        var arduinoModel = require(ROOT_PATH + "service/arduino")(serialPort);
-        return arduinoModel;
+
+        //var COM3 = sm.get('config').Serial.port;
+        try {
+            //return function(callback){
+                var serialPort = sm.get('serial')(sm.get('config').Serial.port);
+                //var serialportFactory = require("serialport");
+                //serialPort.on('open', function(err) {
+                  //  if (err) {
+                  //      console.log("device not found on port: ", sm.get('config').Serial.port);
+                  //  }
+                    var arduinoModel = require(ROOT_PATH + "service/arduino")(serialPort);
+                    return arduinoModel;
+                //});
+             //};
+
+
+        }catch(error){
+            console.log("device not found");
+        }//var serialPort = sm.get('serial')("COM3");
+
     },
     astronaut: function addService(sm){
     	var astronaut = require(ROOT_PATH + "service/astronauts");
     	return astronaut;
     },
     about: function addService(sm){
-        return require(ROOT_PATH + "../routes/about")
+        return require(ROOT_PATH + "../routes/about");
     },
 
     bodyParser: require('body-parser')(),
@@ -77,6 +95,23 @@ var services = {
         return require(ROOT_PATH + 'services/file')(sm.get('fs'));
     },
     fs: require('fs'),
+    io: function addService(sm){
+        var app = sm.get('app');
+        var io = sm.get('socket');
+        return function(server){
+          var sio = io.listen(server,{
+              "transports": ["polling"],
+              "polling duration": 10
+          });
+            return sio;
+        };
+    },
+    httpServer: function addService(sm){
+        var http = sm.get('http');
+        var app = sm.get('app');
+        var httpServer = http.createServer(app);
+        return httpServer;
+    },
     methodOverride: require('method-override')(),
     'model.restEndPoints': function addService(sm) {
         return require(ROOT_PATH + 'models/restEndpoints') (
@@ -101,20 +136,25 @@ var services = {
         var when = sm.get('when');
         
         return function dbConnect(callback){
+            //mongo.Db.connect(dbUri,function(err, db){
         	mongo.Db.connect(dbUri,function(err, db){
-        		
+
 	        		if (err){
-	        			return callback(err);
-	        		}
-	        	mongoDb = db;
-	        	db.collection('mongodb', function(err, testColl){
-	        		
-	        		if (err){
-	        			return callback(err);
-	        		}
-	        		console.log("you are connected to mongodb on heroku :)");
-	        		callback(null,db);
-	        	});
+                        console.log('error when connecting to database');
+	        			//return callback(err);
+	        		}else{
+                        mongoDb = db;
+                        db.collection('mongodb', function(err, testColl){
+
+                            if (err){
+                                //return callback(err);
+                            }
+                            console.log("you are connected to mongodb on heroku :)");
+                            callback(null,db);
+                        });
+                    }
+
+
         	});
         }
     },
@@ -131,11 +171,34 @@ var services = {
         );
     },
     routes: function addService(sm){
-        var routes = require(ROOT_PATH + "../routes");
-        return routes;
+        var routes = []
+        var fs = this.fs;
+        fs.readdirSync(ROOT_PATH + "../routes").forEach(function(file){
+            if (file.substr(-3) === '.js') {
+                routes.push(file);
+            }
+        })
+        var obj = [];
+        var temp = [];
+        routes.forEach(function(route){
+            var r = route.substr(-3);
+            var string = '';
+            var i = 0;
+            while(route[i] !== '.'){
+                string += route[i];
+                i++;
+            }
+            obj[string] = (require(ROOT_PATH + '../routes/' + route))[string];
+        })
+        return obj;
     },
     schema: function addService(sm) {
         return sm.get('mongoose').Schema;
+    },
+
+    socket: function addService(sm) {
+        var socket = require('socket.io');
+        return socket;
     },
 
     users:  function addService(sm) {
