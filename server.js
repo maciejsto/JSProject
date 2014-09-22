3d9e3f418ccc7527d04fdab31d01271e704d474d
maciejsto <maciejsto@gmail.com>
2014-09-22 18:01:10 +0200

    fix depencencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
     , http = require('http');
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(3001);
 var io = require('socket.io').listen(server); // this tells socket.io to use our express server
 
 
@@ -13,6 +13,6 @@ io.sockets.on('connection', function (socket) {
     socket.emit('info', { msg: 'The world is round, there is no up or down.' });
 });
 
-console.log("Express server listening on port 3000");
+console.log("Express server listening on port:", server.address().port);
 
 
--- web.js
+++ web.js
@@ -4,13 +4,29 @@ var sm = require("./src/backend/service/manager")(services);
 var app = sm.get('app');
 var fs = sm.get('fs');
 var usersModel = sm.get('users');
-//var serialPort = sm.get('serial')(sm.get('config').Serial.port);
-var arduinoModel = sm.get('arduinomodel');
+var serialPort = sm.get('serial')(sm.get('config').Serial.port);
+var arduinoModel = sm.get('arduinomodel')(serialPort);
 var routes = sm.get('routes');
 var server = sm.get('httpServer');
 var io = sm.get('io')(server);
-var port = sm.get('config').rest.port;
+var port = sm.get('config').port;
 var controllers = [];   //global variables holding list of controllers files
+var MongoClient = sm.get('mongoClient');
+
+
+
+/*
+app.use(function(req, res , next){
+    req.db = mongoDB;
+});
+*/
+
+/*
+var mongoClient = sm.get('mongo').MongoClient;
+mongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db){
+    console.log('connected.. with mongoClinet to local db..');
+});
+*/
 
 
 fs.readdirSync('./src/backend/controllers').forEach(function (file) {
@@ -22,16 +38,14 @@ fs.readdirSync('./src/backend/controllers').forEach(function (file) {
 var userController = require('./src/backend/controllers/' + controllers[3]);
 var arduinoController = require('./src/backend/controllers/' + controllers[1]);
 
-userController.controller(app, usersModel, io);
+//userController.controller(app, usersModel, io);
 arduinoController.controller(app, arduinoModel, io);
-var Admin = require('./src/backend/controllers/'+ controllers[0]);
+var AdminController = require('./src/backend/controllers/'+ controllers[0]);
 
-console.log(controllers);
+
 
 
 io.on('connection', function (socket) {
-
-
     /*
 serialPort.on('open',function() {
  console.log('serialport opened');
@@ -46,30 +60,19 @@ serialPort.on('open',function() {
     });
 
 */
-
 });
 
-
-
-
-//var ap = require('express')();
-//var server = require('http').Server(ap);
-/*
-var http = sm.get('http');
-var conf = require('./src/backend/config/index')();
-var server = http.createServer(app);
-var io = sm.get('socket');
-*/
-/*
-var socket = io.listen(server,{
-    "transports": ["xhr-polling"],
-    "polling duration": 10
+app.use(function(req,res,next){
+    console.log('something happening here');
+    next();
 });
-*/
-//var io = io.listen(server);
-// Heroku won't actually allow us to use WebSockets
-// so we have to setup polling instead.
-// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+    // configure stuff here
+
+    console.log('configure stuff here..');
+}
 
 //APP SETTINGS//
 app.set('views', __dirname + '/src/front/views');
@@ -79,14 +82,24 @@ app.set('view engine', 'ejs');
 
 
 //ROUTES//
-app.get('/', routes.index);
-app.get('/about', routes.about.dummyFunction);
+app.route('/')
+    .get(routes.index);
+
+app.route('/about')
+    .get(routes.about.dummyFunction);
 //app.get('/arduino', routes.arduino);
 //app.get('*', routes.error);
-app.all("/admin*", Admin.run);
-console.log('name of Admin Controller: ',Admin.name);
+
+app.all("/admin*", function(req,res,next){
+    AdminController.run(req,res,next);
+});
+app.all('/users*', function(req,res,next){
+    userController.run(req,res,next,MongoClient);
+});
+
+console.log('name of Admin Controller: ',AdminController.name);
 
 //START SERVER ON DEDICATED PORT//
 server.listen(port, function(){
-    console.log("http server listening on port: ",port);
+    console.log("http server listening on port: ",server.address().port, server.address().address);
 });
