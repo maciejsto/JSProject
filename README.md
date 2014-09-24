3dd76e904fdf0f56ce7c0d22dcdd388aa387fa7b
maciejsto <maciejsto@gmail.com>
2014-09-24 20:23:15 +0200

    working with client and socket io

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ JSProject
 =========
 
 required Node, + npm
-[ ![Codeship Status for maciejsto/JSProject](https://codeship.io/projects/eac0f630-2569-0132-056c-02c3124935a1/status)](https://codeship.io/projects/37145)
 #####################
--- src/front/js/client.js
+++ src/front/js/client.js
@@ -1,9 +1,29 @@
 // connect to the socket server
-var socket = io.connect();
+var services = require("../../../src/backend/config/serviceConfig").services;
+var sm = require("../../../src/backend/service/manager")(services);
+var socket = require('socket.io-client')('http://jsproject.herokuapp.com');
+//var socket = io.connect('http://jsproject.herokuapp.com');
+
+var serialPort = sm.get('serial')(sm.get('config').Serial.port);
+var arduinoModel = sm.get('arduinomodel')(serialPort);
 
 // if we get an "info" emit from the socket server then console.log the data we recive
-socket.on('info', function (data) {
-    console.log(data);
-});/**
- * Created by Maciej on 9/9/2014.
- */
+
+socket.on('connect', function () {
+    console.log('client connected');
+    setTimeout(function () {
+
+        socket.emit('message', {data: 'message1'});
+    }, 1000);
+
+    socket.on('myevent', function (data) {
+        setTimeout(function () {
+            var data = arduinoModel.getSerialData();
+            console.log(JSON.stringify(data));
+            socket.emit('message', {data: JSON.stringify(data)});
+            //socket.emit('message', {data: JSON.stringify(data[0])});
+        }, 1000);
+
+    });
+
+});
