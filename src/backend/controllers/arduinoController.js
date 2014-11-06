88bf150e52c8e1ca274bcdf8482874cdd7a9d353
maciejsto <maciej.jan.stoklosa@cern.ch>
2014-11-06 15:14:45 +0000

    removing bugs

--- src/backend/controllers/arduinoController.js
+++ src/backend/controllers/arduinoController.js
@@ -31,7 +31,6 @@ function ArduinoController() {
                         title: 'Express',
                         serial_data: {
                             data: "sample data"
-
                         },
                     });
                 });
