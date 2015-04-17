6f5cbaa567e287fa5f7ea809794fb6c7cd907791
maciejsto <maciej.jan.stoklosa@cern.ch>
2015-04-17 20:12:00 +0000

    fixing rpi client

--- client.js
+++ client.js
@@ -16,8 +16,8 @@ var local_string  = "http://localhost:8080";
     //socket = require('socket.io-client')(local_string);
 
 var socket = {};
-    //socket = require('socket.io-client')(heroku_string);
-    socket = require('socket.io-client')(local_string);
+    socket = require('socket.io-client')(heroku_string);
+    //socket = require('socket.io-client')(local_string);
 var gpio = require('rpi-gpio');
 
 /*
