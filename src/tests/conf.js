1c999f0e815e5c2de3c1cdc7ad329f46e506992e
maciej <maciejsto@gmail.com>
2015-08-12 09:54:43 +0000

    fixing angular routes

---
+++ src/tests/conf.js
@@ -0,0 +1,12 @@
+
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  capabilities: {
+  browserName: 'chrome',
+  'chromeOptions': {
+    args: ['--no-sandbox'] 
+  }   
+}
+}
+
